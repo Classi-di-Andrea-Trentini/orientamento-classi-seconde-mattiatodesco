a64c736a8b3df873134ebcccd1fa651d93badf63
mattiatodesco <mattia.todesco@marconirovereto.it>
2025-12-03 11:00:08 +0000

    Aggiungi file di configurazione e implementa la struttura di base per l'applicazione Java

---
+++ Prog2/src/App.java
@@ -0,0 +1,54 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        /*Scanner In = new Scanner(System.in);
+        System.out.println("Inserisci il Numero:");
+        int Numero = In.nextInt();
+        System.out.println("Il tuo numero è: " + Numero);
+        if(Numero == 8){
+            System.out.println("Hai scelto il numero 8");
+
+        } else if (Numero == 7){
+            System.out.println("Hai scelto il numero 7");
+        } else {
+            System.out.println("Hai scelto un numero diverso da 8");
+        }
+
+        switch (Numero){
+            case 1:
+                System.out.println("1");
+                break;
+            case 2:
+                System.out.println("ciao");
+                break;
+            default: 
+                System.out.println("Numero è diverso da 1,2");
+        }
+        boolean var = true;
+        while (var) {
+            String str = In.next();
+            if (str.equals  ("stop")){
+                var = false;
+            }
+        }
+        
+        int[] Vettore = new int[5];
+
+        for (int i = 0; i <Vettore.length; i++) {
+            Vettore[i] = i*2;
+        }
+
+        System.out.println(Vettore[0]);
+
+
+        ArrayList<Integer> Lista = new ArrayList<>(5);
+        int n = Lista.get(2);
+         */
+
+        
+
+        
+    } 
+}
